b4bc06b662db23fb06728315916d8412294ca74d
himateja27 <himatejahima032@gmail.com>
2026-03-13 15:03:53 +0530

    navigate changes happend

--- travels/src/App.jsx
+++ travels/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./deepcomponents/LoginForm";
 import RegisterForm from "./deepcomponents/RegisterForm";
 import BusList from "./deepcomponents/BusList";
 import BusSeats from "./deepcomponents/BusSeats";
 import UserBookings from "./deepcomponents/UserBookings";
 import Wrapper from "./deepcomponents/Wrapper";
+import PrivateRoute from "./deepcomponents/PrivateRoute";
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem("token"));
@@ -30,14 +31,45 @@ const App = () => {
   return (
     <Wrapper handleLogout={handleLogout} token={token}>
       <Routes>
-        <Route path="/buses" element={<BusList />} />
-        <Route path="/seats" element={<BusSeats />} />
+        {/* Default route → go to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Public routes */}
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
-        <Route path="/bus/:busId" element={<BusSeats token={token} />} />
+
+        {/* Protected routes */}
+        <Route
+          path="/buses"
+          element={
+            <PrivateRoute>
+              <BusList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/seats"
+          element={
+            <PrivateRoute>
+              <BusSeats />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/bus/:busId"
+          element={
+            <PrivateRoute>
+              <BusSeats token={token} />
+            </PrivateRoute>
+          }
+        />
         <Route
           path="/my-bookings"
-          element={<UserBookings token={token} userId={userId} />}
+          element={
+            <PrivateRoute>
+              <UserBookings token={token} userId={userId} />
+            </PrivateRoute>
+          }
         />
       </Routes>
     </Wrapper>
